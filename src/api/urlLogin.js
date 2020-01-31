796ebd28cda78793f849dadb070d7358245071c4
bvanh <48560384+bvanh@users.noreply.github.com>
2020-01-31 16:38:27 +0700

    first commit

---
+++ src/api/urlLogin.js
@@ -0,0 +1,6 @@
+const apiLogin={
+    rootApiLogin:'https://api.cms.cubegame.vn/auth/login',
+    getSig:'https://api.cms.cubegame.vn/auth/duo/call?username=',
+    checkLoginFinal:'https://api.cms.cubegame.vn/auth/duo/verify'
+}
+export default apiLogin;
