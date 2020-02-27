d0f638b9e17a5866c9eea52297a31c808c80d42a
bvanh <48560384+bvanh@users.noreply.github.com>
2020-02-27 21:39:41 +0700

    trash

--- src/components/payment/promotion/create/byMoney/menuReward.js
+++ src/components/payment/promotion/create/byMoney/menuReward.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import {
-    Table,
-    Button,
-    Pagination,
-    Input,
-    Row,
-    Col,
-    Radio,
-    DatePicker,
-    Select,
-    TimePicker
+  Table,
+  Button,
+  Pagination,
+  Input,
+  Row,
+  Col,
+  Radio,
+  DatePicker,
+  Select,
+  TimePicker
 } from "antd";
 import { useLazyQuery, useMutation, useQuery } from "@apollo/react-hooks";
 import "../../../../../static/style/promotion.css";
 import {
-    getPromotionType,
-    getEventPaymentType
+  getPromotionType,
+  getEventPaymentType
 } from "../../../../../utils/queryPaymentAndPromoType";
 import { queryGetPlatform } from "../../../../../utils/queryPlatform";
 import { getListPartnerProducts } from "../../../../../utils/queryPartnerProducts";
@@ -24,48 +24,59 @@ const { Option } = Select;
 const { RangePicker } = DatePicker;
 
 const eventMoneyType = [
-    {
-        value: "COIN",
-        description: "Tặng C.Coin"
-    },
-    {
-        value: "INKIND",
-        description: "Tặng quà out game"
-    }
+  {
+    value: "COIN",
+    description: "Tặng C.Coin"
+  },
+  {
+    value: "INKIND",
+    description: "Tặng quà out game"
+  }
 ];
-const eventCointype = [{
-    value: 'ITEM',
-    description: 'Tặng bằng vật phẩm trong game',
-},
-]
+const eventCointype = [
+  {
+    value: "ITEM",
+    description: "Tặng bằng vật phẩm trong game"
+  }
+];
 const MenuRewardEventByMoney = props => {
-    const [eventByMoneyIndex,setEventByMoneyIndex]=useState({
-        type:[],
-    })
-    useQuery(getEventPaymentType, {
-    onCompleted: data => setEventByMoneyIndex({...eventByMoneyIndex,type:data.__type.})
-  })
-    const printEventMoneyType = eventMoneyType.map((val, i) => (
-        <Option key={i} value={val.name}>{val.description}</Option>
-    ))
-    return (
-        <div>
-            <p className="promotion-title-field">Chọn loại hóa đơn</p>
-            <Select
-                style={{ width: 120 }}
-                // onChange={handleChangePlatform}
-                placeholder="-Chọn game-"
-            >
-                {/* {printPlatform} */}
-            </Select>{" "}
-            <span>Hình thức</span>
-            <Select
-                style={{ width: 120 }}
-            // onChange={val => props.handleChaneIndexPromo(val, "setPromoType")}
-            >
-                {printEventMoneyType}
-            </Select>{" "}
-            {/* <span>Server</span>
+  const [eventByMoneyIndex, setEventByMoneyIndex] = useState({
+    typeEvent: [{name:""}]
+  });
+  const {typeEvent}=eventByMoneyIndex
+  useQuery(getEventPaymentType, {
+    onCompleted: data =>
+      setEventByMoneyIndex({
+        ...eventByMoneyIndex,
+        type: data.__type.enumValues
+      })
+  });
+  const printEventMoneyType = eventMoneyType.map((val, i) => (
+    <Option key={i} value={val.name}>
+      {val.description}
+    </Option>
+  ));
+  const printEventType=typeEvent.map((val,index)=>(
+      <Option key={index} value={val.name}>{val.name}</Option>
+  ))
+  return (
+    <div>
+      <p className="promotion-title-field">Chọn loại hóa đơn</p>
+      <Select
+        style={{ width: 120 }}
+        // onChange={handleChangePlatform}
+        placeholder="-Chọn game-"
+      >
+        {printEventType}
+      </Select>{" "}
+      <span>Hình thức</span>
+      <Select
+        style={{ width: 120 }}
+        // onChange={val => props.handleChaneIndexPromo(val, "setPromoType")}
+      >
+        {printEventMoneyType}
+      </Select>{" "}
+      {/* <span>Server</span>
         <Select
           placeholder="-Chọn server-"
           style={{ width: 120 }}
@@ -75,7 +86,7 @@ const MenuRewardEventByMoney = props => {
         >
           {printListServer}
         </Select>{" "} */}
-        </div>
-    );
+    </div>
+  );
 };
 export default MenuRewardEventByMoney;
